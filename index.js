a992178536e6c8bb5f7bd740aa87a14d794815d2
douglasjr1985 <30476669+douglasjr1985@users.noreply.github.com>
2017-07-26 21:49:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,43 @@
- // This loads the environment variables from the .env file
-require('dotenv-extended').load();
+/*-----------------------------------------------------------------------------
+A simple "Hello World" bot for the Microsoft Bot Framework. 
+-----------------------------------------------------------------------------*/
 
 var restify = require('restify');
+//var builder = require('../../core/');
 var builder = require('botbuilder');
-
 
 //=========================================================
 // Bot Setup
 //=========================================================
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 5000, function () {
-   console.log('%s listening to %s', server.name, server.url);
-});
+
+
+  
 // Create chat bot
 var connector = new builder.ChatConnector({
-      appId: "0223da35-1906-41ae-867e-5d5aed8f1003",
+    appId: "0223da35-1906-41ae-867e-5d5aed8f1003",
     appPassword: "HqPriUid8XLBqDvvRwkcJ7k"
 });
 var bot = new builder.UniversalBot(connector);
 
-server.post('/api/messages', connector.listen());
-//Bot on
-bot.on('contactRelationUpdate', function (message) {
-    if (message.action === 'add') {
-        var name = message.user ? message.user.name : null;
-        var reply = new builder.Message()
-                .address(message.address)
-                .text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos.", name || 'there');
-        bot.send(reply);
-    } else {
-        // delete their data
-    }
-});
-bot.on('typing', function (message) {
-  // User is typing
-});
-bot.on('deleteUserData', function (message) {
-    // User asked to delete their data
-});
+
 //=========================================================
 // Bots Dialogs
 //=========================================================
-String.prototype.contains = function(content){
-  return this.indexOf(content) !== -1;
-}
 
-bot.dialog('/', function (session) {
-    if(session.message.text.toLowerCase().contains('oi')){
-      session.send('Hey, How are you?');
-      }else if(session.message.text.toLowerCase().contains('bom dia')){
-        session.send('Bom dia');
-      }else if(session.message.text.toLowerCase().contains('help1')){
-        session.send('How can I help you?');
-      }
-      
-      else{
-        session.send('000Sorry I don understand you...');
-      }
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', connector.listen());
+server.port = process.env.port || process.env.PORT || 80;
+server.host = process.env.port || '0.0.0.0';
+server.listen(server.port,server.host, function () {
+       console.log('%s FDICbotmbf listening to %s', server.name, server.url);
 });
 
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
 
-
+server.get('/', restify.serveStatic({
+    directory: __dirname,
+    default: '/index.html'
+}));
