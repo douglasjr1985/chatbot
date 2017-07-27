4f9f8126c2d8d5ab32d419b5c83002293e1d4885
douglasjr1985 <30476669+douglasjr1985@users.noreply.github.com>
2017-07-26 21:31:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var builder = require('botbuilder');
 //=========================================================
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 80, function () {
+server.listen(process.env.port || process.env.PORT || 5000, function () {
    console.log('%s listening to %s', server.name, server.url);
 });
 // Create chat bot
